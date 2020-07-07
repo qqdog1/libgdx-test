1e436cf0d5f4e8221a4f562b3d3f798d358c96e2
Shawn <qqdog1@yahoo.com.tw>
2020-07-07 15:21:19 +0800

    1. echo hole

--- core/src/name/qd/game/test/screen/LogoScreen.java
+++ core/src/name/qd/game/test/screen/LogoScreen.java
@@ -5,12 +5,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-import name.qd.game.test.constant.Constants;
 import name.qd.game.test.constant.ScreenType;
 import name.qd.game.test.utils.MaterialCreator;
 
 public class LogoScreen extends GameScreen {
     private Animation animation;
+    private Texture echohole;
 //    private final Sound sound;
 
     private float stateTime;
@@ -22,9 +22,10 @@ public class LogoScreen extends GameScreen {
 
     public LogoScreen() {
         animation = MaterialCreator.createAnimation(new Texture("pic/logo.png"), 0, 0, 256, 256, 5, 0.2f);
+        echohole = new Texture("pic/echohole.png");
 
-        scaleWidth = ((TextureRegion)animation.getKeyFrame(0)).getTexture().getWidth() * SCALE_RATE / 5;
-        scaleHeight = ((TextureRegion)animation.getKeyFrame(0)).getTexture().getHeight() * SCALE_RATE;
+        scaleWidth = 256 * SCALE_RATE;
+        scaleHeight = 256 * SCALE_RATE;
         x = (WIDTH - scaleWidth) / 2;
         y = HEIGHT / 2 - scaleHeight / 3;
     }
@@ -45,6 +46,9 @@ public class LogoScreen extends GameScreen {
 
         spriteBatch.begin();
         spriteBatch.draw((TextureRegion)animation.getKeyFrame(stateTime), x, y, scaleWidth, scaleHeight);
+        if(animation.isAnimationFinished(stateTime)) {
+            spriteBatch.draw(echohole, (WIDTH - echohole.getWidth() * SCALE_RATE) / 2, y - echohole.getHeight() * SCALE_RATE, echohole.getWidth() * SCALE_RATE, echohole.getHeight() * SCALE_RATE);
+        }
         spriteBatch.end();
     }
 
